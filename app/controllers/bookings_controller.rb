c4a3ac1caa7901f9618714a05c24fb26050a41a7
Latoya Alford <lat.jamison@gmail.com>
2023-05-04 13:24:41 -0400

    fixed booking status line

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,18 +10,19 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @booking.pokemon = @pokemon
     @booking.user = current_user
-    # @booking.status = true
+    @booking.booking_status = "pending"
     authorize @booking #we authorize the ability to create a pokemon booking
-    if @booking.end_date && @booking.start_date
-      @booking_prize = (@booking.end_date - @booking.start_date).to_f * @booking.pokemon.price.to_f
+    if @booking.end_date && @booking.start_date #need clarity on this line
+      @booking_price = (@booking.end_date - @booking.start_date).to_f * @booking.pokemon.price.to_f
     else
-      @booking_prize = 0
+      @booking_price = 0
     end
     if @booking.save
       redirect_to root_path
     else
       redirect_to bed_path(@pokemon)
     end
+    raise
   end
 
   private
