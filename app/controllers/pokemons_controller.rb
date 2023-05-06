1bc128cbe6a88f083ddaab0e16807209f85bf530
Latoya Alford <lat.jamison@gmail.com>
2023-05-06 12:30:56 -0400

    created pokemon index page that list pokemons

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,7 +1,25 @@
 class PokemonsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :show ]
+  skip_before_action :authenticate_user!, only: [ :show,:index ]
+  before_action :find_pokemon, only: [:show, :destroy, :edit, :update]
+
+  def index
+    @pokemons = policy_scope(Pokemon)
+  end
+
   def show
     @pokemon = Pokemon.find(params[:id])
     authorize(@pokemon)
+  end
+
+  def destroy
+    @pokemon.destroy
+    raise
+    redirect_to pokemons_path, status: :see_other
+  end
+
+  private
+
+  def find_pokemon
+    @pokemon = Pokemon.find(params[:id])
   end
 end
